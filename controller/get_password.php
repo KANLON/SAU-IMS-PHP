21e309d09035808e176b5ca1042f26421274e269
AponeWorker <aponework@outlook.com>
2016-11-05 20:08:44 +0800

    clearAll

--- controller/get_password.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * 找回密码
- * User: APone
- * Date: 2016/10/29
- * Time: 11:08
- */
--- controller/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * 用户登出
- * User: APone
- * Date: 2016/10/29
- * Time: 11:08
- */
